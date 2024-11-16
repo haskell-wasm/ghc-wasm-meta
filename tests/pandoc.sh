9c4525d3f04ea278d0792f7e84abe34a67be7356
Cheng Shao <terrorjack@type.dance>
2024-11-16 18:23:43 +0000

    Bump

--- tests/pandoc.sh
+++ tests/pandoc.sh
@@ -7,4 +7,6 @@ export PATH=$PATH:/opt/toolchain/bin
 pushd "$(mktemp -d)"
 curl -L https://github.com/TerrorJack/pandoc/archive/refs/heads/wasm.tar.gz | tar xz --strip-components=1
 wasm32-wasi-cabal build pandoc-cli
+wasmtime run --dir "$PWD"::/ -- "$(find dist-newstyle -type f -name pandoc.wasm)" README.md -o README.rst
+head --lines=20 README.rst
 popd
