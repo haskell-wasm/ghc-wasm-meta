3557f5458854ad41791eca25a196568d77821916
Cheng Shao <terrorjack@type.dance>
2024-11-23 20:29:44 +0000

    ci: use haskell-wasm upstream for pandoc

--- tests/pandoc.sh
+++ tests/pandoc.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 export PATH=$PATH:/opt/toolchain/bin
 
 pushd "$(mktemp -d)"
-curl -L https://github.com/TerrorJack/pandoc/archive/refs/heads/wasm.tar.gz | tar xz --strip-components=1
+curl -L https://github.com/haskell-wasm/pandoc/archive/refs/heads/wasm.tar.gz | tar xz --strip-components=1
 wasm32-wasi-cabal build pandoc-cli
 wasmtime run --dir "$PWD"::/ -- "$(find dist-newstyle -type f -name pandoc.wasm)" README.md -o README.rst
 head --lines=20 README.rst
