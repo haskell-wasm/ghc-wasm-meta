623aa5b1778d2d8399f6b51f2fd6762352072dba
Cheng Shao <terrorjack@type.dance>
2024-11-23 21:06:47 +0000

    ci: add humblr test to 9.12

--- tests/humblr.sh
+++ tests/humblr.sh
@@ -5,5 +5,17 @@ set -euo pipefail
 pushd "$(mktemp -d)"
 curl -L https://github.com/konn/humblr/archive/refs/heads/main.tar.gz | tar xz --strip-components=1
 rm -f ./*.freeze
+cp $CI_PROJECT_DIR/cabal.project.local cabal-wasm.project.local
+sed -i \
+  -e 's@amesgen/miso@dmjio/miso@' \
+  -e 's/bb9ce9a3dd03a7c1ac945943f65955ab10a53011/9d15de0001b7afb98e0983e7bc09ca81faa681fd/' \
+  cabal-common.project
+sed -i \
+  -e '/with-compiler:/d' \
+  -e '/jobs:/d' \
+  -e '/optimization:/d' \
+  -e 's@amesgen/miso@dmjio/miso@' \
+  -e 's/bb9ce9a3dd03a7c1ac945943f65955ab10a53011/9d15de0001b7afb98e0983e7bc09ca81faa681fd/' \
+  cabal-wasm.project
 wasm32-wasi-cabal --project-file=cabal-wasm.project build all
 popd
