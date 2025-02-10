7338623b5349f2a15712256348116db61e4fe8da
Cheng Shao <terrorjack@type.dance>
2025-02-10 11:04:34 +0000

    Fix tests

--- tests/miso-ormolu.sh
+++ tests/miso-ormolu.sh
@@ -3,17 +3,13 @@
 set -euo pipefail
 
 pushd "$(mktemp -d)"
-curl -L https://github.com/sass/dart-sass/releases/download/1.81.0/dart-sass-1.81.0-linux-x64.tar.gz | tar xz --strip-components=1
+curl -L https://github.com/sass/dart-sass/releases/download/1.83.4/dart-sass-1.83.4-linux-x64.tar.gz | tar xz --strip-components=1
 export PATH=$PATH:$PWD:/opt/toolchain/bin
 popd
 
 pushd "$(mktemp -d)"
 curl -L https://github.com/tweag/ghc-wasm-miso-examples/archive/refs/heads/main.tar.gz | tar xz --strip-components=1
 cp $CI_PROJECT_DIR/cabal.project.local .
-echo "source-repository-package" >> cabal.project.local
-echo "  type: git" >> cabal.project.local
-echo "  location: https://github.com/dmjio/miso.git" >> cabal.project.local
-echo "  tag: 9d15de0001b7afb98e0983e7bc09ca81faa681fd" >> cabal.project.local
 pushd frontend
 ./build.sh
 popd
@@ -27,10 +23,14 @@ pushd frontend
 popd
 popd
 
-if [[ "$FLAVOUR" == 9.10 ]]; then
+if [[ "$FLAVOUR" == 9.12 ]]; then
   pushd "$(mktemp -d)"
-  curl -L https://hackage.haskell.org/package/ormolu-0.7.7.0/ormolu-0.7.7.0.tar.gz | tar xz --strip-components=1
+  curl -L https://hackage.haskell.org/package/ormolu-0.8.0.0/ormolu-0.8.0.0.tar.gz | tar xz --strip-components=1
   cp $CI_PROJECT_DIR/cabal.project.local .
+  sed -i \
+    -e '/-threaded/d' \
+    -e '/-with-rtsopts/d' \
+    ormolu.cabal
   wasm32-wasi-cabal build -finternal-bundle-fixities
   popd
 fi
