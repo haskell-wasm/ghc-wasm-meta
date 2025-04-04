0de79c61a439539ed26f68a2c419430f02e4c15e
Cheng Shao <terrorjack@type.dance>
2025-04-04 02:59:00 +0000

    ci: test miso todomvc in playwright

--- tests/miso-ormolu.sh
+++ tests/miso-ormolu.sh
@@ -3,15 +3,23 @@
 set -euo pipefail
 
 pushd "$(mktemp -d)"
-curl -L https://github.com/sass/dart-sass/releases/download/1.83.4/dart-sass-1.83.4-linux-x64.tar.gz | tar xz --strip-components=1
+curl -L https://github.com/sass/dart-sass/releases/download/1.86.2/dart-sass-1.86.2-linux-x64.tar.gz | tar xz --strip-components=1
 export PATH=$PATH:$PWD:/opt/toolchain/bin
 popd
 
 pushd "$(mktemp -d)"
-curl -L https://github.com/tweag/ghc-wasm-miso-examples/archive/refs/heads/main.tar.gz | tar xz --strip-components=1
+curl -f -L --retry 5 https://github.com/tweag/ghc-wasm-miso-examples/archive/refs/heads/debug.tar.gz | tar xz --strip-components=1
 cp $CI_PROJECT_DIR/cabal.project.local .
 pushd frontend
 ./build.sh
+export TODOMVC_DIST_DIR=$PWD/dist
+pushd "$(mktemp -d)"
+curl -f -L --retry 5 https://github.com/haskell-wasm/playwright/archive/refs/heads/release-1.51.tar.gz | tar xz --strip-components=1
+cd examples/todomvc
+npm install
+npx playwright install --with-deps
+npx playwright test --workers=$CPUS
+popd
 popd
 popd
 
