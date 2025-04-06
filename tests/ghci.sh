956e22ab215034d954287ccb4c38c54184ed204e
Cheng Shao <terrorjack@type.dance>
2025-04-06 14:44:31 +0000

    ci: test ghci browser mode

--- tests/ghci.sh
+++ tests/ghci.sh
@@ -2,4 +2,18 @@
 
 set -euo pipefail
 
-echo -e 'foreign import javascript "Promise.resolve(114514)" x :: IO Int\nx\n' | wasm32-wasi-ghc --interactive -package ghc -package Cabal
+GHCI_SCRIPT='foreign import javascript "new Promise(res => setTimeout(res, 1024, 114514))" x :: IO Int\nx\n'
+
+echo -e "$GHCI_SCRIPT" | wasm32-wasi-ghc --interactive -package ghc -package Cabal
+
+FIREFOX_PATH=$(type -P firefox || echo "")
+
+if [[ $FIREFOX_PATH != "" ]]; then
+  echo -e "$GHCI_SCRIPT" | wasm32-wasi-ghc --interactive -fghci-browser -fghci-browser-puppeteer-launch-opts="{\"browser\":\"firefox\",\"executablePath\":\"$FIREFOX_PATH\"}"
+fi
+
+CHROME_PATH=$(type -P google-chrome || type -P chromium || echo "")
+
+if [[ $CHROME_PATH != "" ]]; then
+  echo -e "$GHCI_SCRIPT" | wasm32-wasi-ghc --interactive -fghci-browser -fghci-browser-puppeteer-launch-opts="{\"browser\":\"chrome\",\"protocol\":\"webDriverBiDi\",\"executablePath\":\"$CHROME_PATH\",\"args\":[\"--no-sandbox\"]}"
+fi
