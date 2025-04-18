f388df0eab5d55a757e7b9142873d172b17e3781
Cheng Shao <terrorjack@type.dance>
2025-04-18 19:46:34 +0000

    ci: bump playwright/sass

--- tests/miso-ormolu.sh
+++ tests/miso-ormolu.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 pushd "$(mktemp -d)"
-curl -L https://github.com/sass/dart-sass/releases/download/1.86.2/dart-sass-1.86.2-linux-x64.tar.gz | tar xz --strip-components=1
+curl -f -L --retry 5 https://github.com/sass/dart-sass/releases/download/1.86.3/dart-sass-1.86.3-linux-x64.tar.gz | tar xz --strip-components=1
 export PATH=$PATH:$PWD:/opt/toolchain/bin
 popd
 
@@ -14,10 +14,10 @@ pushd frontend
 ./build.sh
 export TODOMVC_DIST_DIR=$PWD/dist
 pushd "$(mktemp -d)"
-curl -f -L --retry 5 https://github.com/haskell-wasm/playwright/archive/refs/heads/release-1.51.tar.gz | tar xz --strip-components=1
+curl -f -L --retry 5 https://github.com/haskell-wasm/playwright/archive/refs/heads/release-1.52.tar.gz | tar xz --strip-components=1
 cd examples/todomvc
 npm install
-npx playwright install --with-deps
+npx playwright install --with-deps --no-shell
 npx playwright test --retries=2 --workers=$(($CPUS > 8 ? 8 : $CPUS))
 popd
 popd
