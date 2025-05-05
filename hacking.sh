b8bc33f4e30f6bb9cb0ceabcc108a77a90ebadf3
Cheng Shao <terrorjack@type.dance>
2025-05-05 20:22:11 +0000

    ci: bump ci-images

--- hacking.sh
+++ hacking.sh
@@ -11,6 +11,6 @@ exec podman run -it --rm \
   --network host \
   --volume "$PWD":/workspace \
   --workdir /workspace \
-  registry.gitlab.haskell.org/ghc/ci-images/x86_64-linux-ubuntu24_04:59da90988f9f3caa36572bf47d5f78704a969dea \
+  registry.gitlab.haskell.org/ghc/ci-images/x86_64-linux-ubuntu24_04:6e9f8f17086e56e83adae4a8a9d63e2fec3cb6c7 \
   bash -c \
   "apt update && apt full-upgrade -y && apt install -y bash-completion nano zstd && cp /etc/skel/{.bash_logout,.bashrc,.profile} /root && PREFIX=/tmp/.ghc-wasm ./setup.sh && . /tmp/.ghc-wasm/env && exec bash -i"
