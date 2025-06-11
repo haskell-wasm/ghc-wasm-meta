e19f0b853657bbd0262e119ad31d04ca03452d22
Cheng Shao <terrorjack@type.dance>
2025-06-11 20:01:38 +0000

    ci: update playwright test

--- tests/miso-reflex-examples.sh
+++ tests/miso-reflex-examples.sh
@@ -3,25 +3,29 @@
 set -euo pipefail
 
 pushd "$(mktemp -d)"
-curl -f -L https://github.com/sass/dart-sass/releases/download/1.88.0/dart-sass-1.88.0-linux-x64.tar.gz | tar xz --strip-components=1
+curl -f -L https://github.com/sass/dart-sass/releases/download/1.89.2/dart-sass-1.89.2-linux-x64.tar.gz | tar xz --strip-components=1
 export PATH=$PATH:$PWD:/opt/toolchain/bin
 popd
 
 pushd "$(mktemp -d)"
-curl -f -L --retry 5 https://github.com/tweag/ghc-wasm-miso-examples/archive/refs/heads/debug.tar.gz | tar xz --strip-components=1
+curl -f -L --retry 5 https://github.com/tweag/ghc-wasm-miso-examples/archive/refs/heads/main.tar.gz | tar xz --strip-components=1
 cp $CI_PROJECT_DIR/cabal.project.local .
+printf "package *\n  optimization: 2\n" >> cabal.project.local
+printf "package ghc-wasm-miso-examples\n  ghc-options: -debug\n" >> cabal.project.local
 pushd frontend
 ./build.sh
 export TODOMVC_DIST_DIR=$PWD/dist
 pushd "$(mktemp -d)"
-curl -f -L --retry 5 https://github.com/haskell-wasm/playwright/archive/refs/heads/release-1.52.tar.gz | tar xz --strip-components=1
+curl -f -L --retry 5 https://github.com/haskell-wasm/playwright/archive/refs/heads/release-1.53.tar.gz | tar xz --strip-components=1
 cd examples/todomvc
 npm install
 npx playwright install --with-deps --no-shell
-npx playwright test --retries=2 --workers=$(($CPUS > 8 ? 8 : $CPUS))
+npx playwright test --reporter=list --retries=2 --workers=$(($CPUS > 8 ? 8 : $CPUS))
 popd
 popd
 popd
+
+exit
 
 pushd "$(mktemp -d)"
 curl -L https://github.com/tweag/ghc-wasm-reflex-examples/archive/refs/heads/main.tar.gz | tar xz --strip-components=1
