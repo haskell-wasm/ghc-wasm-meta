c6af531c70d8b14d0ebe34516b28c37df6f6a041
Cheng Shao <terrorjack@type.dance>
2025-06-11 20:02:14 +0000

    util: improve hacking.sh

--- hacking.sh
+++ hacking.sh
@@ -2,13 +2,13 @@
 
 exec podman run -it --rm \
   --env CI_PROJECT_DIR=/workspace \
+  --env CPUS=48 \
   --env FLAVOUR="$1" \
   --env PLAYWRIGHT=1 \
   --user root \
   --init \
   --privileged \
   --tmpfs /tmp:exec \
-  --network host \
   --volume "$PWD":/workspace \
   --workdir /workspace \
   registry.gitlab.haskell.org/ghc/ci-images/x86_64-linux-ubuntu24_04:6e9f8f17086e56e83adae4a8a9d63e2fec3cb6c7 \
