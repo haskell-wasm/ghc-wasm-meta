7f63db9c2b12d11c59b6ce3a91d23efaa7b4accb
Cheng Shao <terrorjack@type.dance>
2025-08-26 11:45:45 +0000

    ci: fix tasty-bench

--- tests/misc.sh
+++ tests/misc.sh
@@ -50,6 +50,7 @@ popd
 
 pushd "$(mktemp -d)"
 curl -f -L --retry 5 https://github.com/Bodigrim/tasty-bench/archive/refs/heads/master.tar.gz | tar xz --strip-components=1
+cp $CI_PROJECT_DIR/cabal.project.local .
 wasm32-wasi-cabal build --enable-benchmarks
 $CROSS_EMULATOR $(find . -type f -name bench-fibo.wasm)
 popd
