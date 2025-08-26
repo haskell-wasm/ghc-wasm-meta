e931b6aff2aee44ee75d8e9f44a26bbb2a5e37de
Cheng Shao <terrorjack@type.dance>
2025-08-26 11:44:33 +0000

    ci: bump WasmEdge/toywasm/bun

--- tests/pandoc.sh
+++ tests/pandoc.sh
@@ -18,7 +18,7 @@ head --lines=20 README.rst
 rm README.rst
 
 pushd "$(mktemp -d)"
-curl -f -L --retry 5 https://github.com/WasmEdge/WasmEdge/releases/download/0.14.1/WasmEdge-0.14.1-ubuntu20.04_x86_64.tar.gz | tar xz --strip-components=1
+curl -L https://github.com/WasmEdge/WasmEdge/releases/download/0.15.0/WasmEdge-0.15.0-manylinux_2_28_x86_64.tar.xz | tar xJ
 export PATH=$PATH:$PWD/bin
 popd
 
@@ -27,7 +27,7 @@ head --lines=20 README.rst
 rm README.rst
 
 pushd "$(mktemp -d)"
-curl -f -L --retry 5 https://github.com/yamt/toywasm/releases/download/v68.0.0/toywasm-v68.0.0-full-ubuntu-22.04-amd64.tgz | tar xz
+curl -f -L --retry 5 https://github.com/yamt/toywasm/releases/download/v71.0.0/toywasm-v71.0.0-full-ubuntu-22.04-amd64.tgz | tar xz
 export PATH=$PATH:$PWD/bin
 popd
 
@@ -36,7 +36,7 @@ head --lines=20 README.rst
 rm README.rst
 
 pushd "$(mktemp -d)"
-curl -f -L --retry 5 https://github.com/oven-sh/bun/releases/download/bun-v1.2.17/bun-linux-x64.zip -O
+curl -f -L --retry 5 https://github.com/oven-sh/bun/releases/download/bun-v1.2.21/bun-linux-x64.zip -O
 unzip bun-linux-x64.zip
 BUN=$PWD/bun-linux-x64/bun
 popd
