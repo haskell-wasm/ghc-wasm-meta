73972230a5b5500cad67ddfceafaa983f8a67548
Cheng Shao <terrorjack@type.dance>
2025-09-21 18:25:55 +0000

    ci: fix agda

--- tests/agda.sh
+++ tests/agda.sh
@@ -5,9 +5,11 @@ set -euo pipefail
 export PATH=$PATH:/opt/toolchain/bin
 
 pushd "$(mktemp -d)"
-git clone --branch=v2.7.0.1 --depth=1 --recurse-submodules --shallow-submodules --jobs=2 https://github.com/agda/agda.git .
+git clone --branch=v2.8.0 --depth=1 --recurse-submodules --shallow-submodules --jobs=2 https://github.com/agda/agda.git .
 curl -L https://raw.githubusercontent.com/agda-web/agda-wasm-dist/refs/heads/master/agda-wasm.patch | git apply
 cp $CI_PROJECT_DIR/cabal.project.local .
+echo "package Agda" >> cabal.project.local
+echo "  flags: +use-xdg-data-home" >> cabal.project.local
 echo "package unix-compat" >> cabal.project.local
 echo "  ghc-options: -optc-Wno-error=implicit-function-declaration" >> cabal.project.local
 wasm32-wasi-cabal build agda
