32e3243de87fdb7b0a8ae9cb649b8c650c8ae9fd
Cheng Shao <terrorjack@type.dance>
2025-10-11 12:46:59 +0000

    deps: bump puppeteer-core

--- pkgs/npm-deps.nix
+++ pkgs/npm-deps.nix
@@ -12,7 +12,7 @@ buildNpmPackage {
     "package.json"
     "package-lock.json"
   ];
-  npmDepsHash = "sha512-LDY7XpeERmnce3AtcYCHBFrbQuumtli06g6X6Eno5SSNQU0Y34S/kxIZ0BxgEkHcRVQs8/IgnLU8YSlF3QBDNg==";
+  npmDepsHash = "sha512-gbCd3hlPyA4Y+2bPolh6jeEGOuWradZAKoqLKmVAXBjxIr9NTbyfREXH2D5D3vI3vYlzF6QpI8p+MpiL4TfqCA==";
 
   dontNpmBuild = true;
   dontNpmInstall = true;
