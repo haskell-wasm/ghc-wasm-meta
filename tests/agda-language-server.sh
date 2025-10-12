f4ffff648a770d9d46d9cf2ace9eb7e5c33c5f63
Cheng Shao <terrorjack@type.dance>
2025-10-12 16:48:42 +0000

    ci: fix agda-language-server test

--- tests/agda-language-server.sh
+++ tests/agda-language-server.sh
@@ -6,7 +6,14 @@ export PATH=$PATH:/opt/toolchain/bin
 
 pushd "$(mktemp -d)"
 
-git clone --depth=1 --recurse-submodules --shallow-submodules https://github.com/agda/agda-language-server.git .
+git clone --depth=1 https://github.com/agda/agda-language-server.git .
+sed -i -e "s|git@github.com:agda-web|https://github.com/agda-web|" .gitmodules
+git submodule update --init --recursive --depth=1 --jobs=4
+
+pushd wasm-submodules/agda
+git fetch --depth 1 origin agda-web/release-v2.8.0
+git checkout FETCH_HEAD
+popd
 
 pushd wasm-submodules/network
 autoreconf -i
@@ -15,7 +22,7 @@ popd
 mv cabal.project.wasm32 cabal.project
 echo "tests: True" >> cabal.project.local
 echo "package agda-language-server" >> cabal.project.local
-echo "  flags: +Agda-2-7-0" >> cabal.project.local
+echo "  flags: +Agda-2-8-0" >> cabal.project.local
 cat "$CI_PROJECT_DIR/cabal.project.local" >> cabal.project.local
 
 wasm32-wasi-cabal build
