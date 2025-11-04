561818bb3900316bfe1ba898112f466ad409a13b
Cheng Shao <terrorjack@type.dance>
2025-11-04 18:55:04 +0000

    ci: consolidate jsaddle-wasm/miso-todomvc test

--- tests/jsaddle-wasm.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-pushd "$(mktemp -d)"
-curl -f -L https://github.com/amesgen/jsaddle-wasm/archive/refs/heads/main.tar.gz | tar xz --strip-components=1
-cp "$CI_PROJECT_DIR/cabal.project.local" .
-wasm32-wasi-cabal build all
-popd
---
+++ tests/web/jsaddle-wasm.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+cd "$(mktemp -d)"
+
+git clone --depth=1 --recurse-submodules --shallow-submodules https://github.com/haskell-wasm/jsaddle-wasm.git .
+cp "$CI_PROJECT_DIR"/cabal.project.local .
+wasm32-wasi-cabal build miso-todomvc --dry-run
+cp -r "$(find dist-newstyle -type d -name static)" public
+wasm32-wasi-cabal install miso-todomvc --install-method=copy --installdir=public
+"$(wasm32-wasi-ghc --print-libdir)"/post-link.mjs --input public/app.wasm --output public/ghc_wasm_jsffi.js
+export TODOMVC_DIST_DIR=$PWD/public
+
+git clone --depth=1 https://github.com/haskell-wasm/playwright.git
+cd playwright/examples/todomvc
+npm install
+npx playwright install --with-deps --no-shell
+npx playwright test --reporter=list --workers=$(($CPUS > 8 ? 8 : $CPUS))
