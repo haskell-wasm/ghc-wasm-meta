a92a4ef3b34dbe9aab26f051bbd25d35a1b8769f
Cheng Shao <terrorjack@type.dance>
2026-01-14 22:33:08 +0000

    ci: fix hfsnotify test

--- tests/hfsnotify.sh
+++ tests/hfsnotify.sh
@@ -7,6 +7,7 @@ export PATH=$PATH:/opt/toolchain/bin
 pushd "$(mktemp -d)"
 curl -f -L https://github.com/haskell-fswatch/hfsnotify/archive/refs/heads/master.tar.gz | tar xz --strip-components=1
 cp "$CI_PROJECT_DIR/cabal.project.local" .
+echo "constraints: monad-control >= 1.0.3.1" >> cabal.project.local
 wasm32-wasi-cabal build all
 wasm32-wasi-cabal list-bin exe:example
 $CROSS_EMULATOR "$(wasm32-wasi-cabal list-bin exe:example)"
