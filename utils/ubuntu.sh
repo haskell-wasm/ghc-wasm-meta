f96a9ef4591c56c5965078f3da24af6de5027d14
Cheng Shao <terrorjack@type.dance>
2026-01-14 20:10:53 +0000

    ci: bump ci-images

--- utils/ubuntu.sh
+++ utils/ubuntu.sh
@@ -11,6 +11,6 @@ exec podman run -it --rm \
   --volume ~/workspace/.codex:/root/.codex \
   --volume "$PWD":/workspace \
   --workdir /workspace \
-  registry.gitlab.haskell.org/ghc/ci-images/x86_64-linux-ubuntu24_04:3cd01b1eef441493a456de54fb3505c74e7cf914 \
+  registry.gitlab.haskell.org/ghc/ci-images/x86_64-linux-ubuntu24_04:7c514a3eaeab072e8789173e583a6ce5b26f0685 \
   bash -c \
   "apt update && apt full-upgrade -y && apt install -y bash-completion nano zstd && cp /etc/skel/{.bash_logout,.bashrc,.profile} /root && PREFIX=/tmp/.ghc-wasm ./setup.sh && . /tmp/.ghc-wasm/env && exec bash -i"
