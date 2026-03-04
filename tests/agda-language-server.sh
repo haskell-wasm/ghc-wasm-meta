27d2eb9f151c20ba619e4c64fc9f9d02bda99565
Cheng Shao <terrorjack@type.dance>
2026-03-04 18:18:53 +0100

    ci: fix agda-language-server test

--- tests/agda-language-server.sh
+++ tests/agda-language-server.sh
@@ -4,10 +4,12 @@ set -euo pipefail
 
 export PATH=$PATH:/opt/toolchain/bin
 
-pushd "$(mktemp -d)"
+workdir=$(mktemp -d)
+trap 'rm -rf "$workdir"' EXIT
+
+cd "$workdir"
 
 git clone --depth=1 https://github.com/agda/agda-language-server.git .
-sed -i -e "s|git@github.com:agda-web|https://github.com/agda-web|" .gitmodules
 git submodule update --init --recursive --depth=1 --jobs=4
 
 pushd wasm-submodules/agda
@@ -20,12 +22,12 @@ autoreconf -i
 popd
 
 mv cabal.project.wasm32 cabal.project
-echo "tests: True" >> cabal.project.local
+echo "tests: False" >> cabal.project.local
+# lsp-2.8.0.0 -> websockets -> entropy, custom Setup
+echo "constraints: lsp < 2.8.0.0" >> cabal.project.local
 echo "package agda-language-server" >> cabal.project.local
 echo "  flags: +Agda-2-8-0" >> cabal.project.local
 cat "$CI_PROJECT_DIR/cabal.project.local" >> cabal.project.local
 
 wasm32-wasi-cabal build
 $CROSS_EMULATOR "$(find . -name als.wasm)" --help
-
-popd
